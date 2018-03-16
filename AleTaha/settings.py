f8d644019519a8748c234ec6dd6ab6974ed002e7
DESKTOP-BPVM8T8\asus <navid.azami@gmail.com>
2018-03-16 16:37:43 +0330

    fixed permissions

--- AleTaha/settings.py
+++ AleTaha/settings.py
@@ -130,9 +130,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
     ),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    )
+    # 'DEFAULT_PERMISSION_CLASSES': (
+    #     'rest_framework.permissions.IsAuthenticated',
+    # )
 }
 
 MEDIA_URL = '/media/'
