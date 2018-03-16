fa46858a874443f7ef83a7c71db9c5d4fc40adeb
DESKTOP-BPVM8T8\asus <navid.azami@gmail.com>
2018-03-16 16:28:40 +0330

    fixed authentication

--- AleTaha/settings.py
+++ AleTaha/settings.py
@@ -128,7 +128,6 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
